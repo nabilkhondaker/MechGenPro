53b786cc8df85c9c7a57838f60c24bc14051fd51
𝙽𝚊𝚋𝚒𝚕 𝙺𝚑𝚘𝚗𝚍𝚊𝚔𝚎𝚛 𝙰. <138402618+nabilkhondaker@users.noreply.github.com>
2026-07-12 11:36:47 -0700

    Implement Logger module with log levels and methods

--- src/utils/Logger.lua
+++ src/utils/Logger.lua
@@ -1,1 +1,28 @@
+local Logger = {
+    LEVELS = { INFO = 1, WARN = 2, ERROR = 3 },
+    currentLevel = 1
+}
 
+function Logger.log(level, msg)
+    if level >= Logger.currentLevel then
+        local prefix = "[INFO] "
+        if level == Logger.LEVELS.WARN then prefix = "[WARN] " end
+        if level == Logger.LEVELS.ERROR then prefix = "[ERROR] " end
+        
+        -- Write to console and append to a debug file
+        local fullMsg = os.date("%H:%M:%S ") .. prefix .. msg
+        print(fullMsg)
+        
+        local f = io.open("mechgen_debug.log", "a")
+        if f then
+            f:write(fullMsg .. "\n")
+            f:close()
+        end
+    end
+end
+
+function Logger.info(msg) Logger.log(Logger.LEVELS.INFO, msg) end
+function Logger.warn(msg) Logger.log(Logger.LEVELS.WARN, msg) end
+function Logger.error(msg) Logger.log(Logger.LEVELS.ERROR, msg) end
+
+return Logger
